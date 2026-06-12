9c103220ab39ad7760d5aa2c395c7bfc93b54508
iam-i-j-k <irfanjankhan7860@gmail.com>
2026-06-12 22:39:29 +0530

    api

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -52,7 +52,7 @@ const App = () => {
     }
     setFormStatus("sending");
     try {
-      const res = await fetch("https://dietician-cafh.onrender.com/contact", {
+      const res = await fetch("https://dietician-cafh.onrender.com/api/contact", {
         method: "POST",
         headers: { "Content-Type": "application/json" },
         body: JSON.stringify(formData),
